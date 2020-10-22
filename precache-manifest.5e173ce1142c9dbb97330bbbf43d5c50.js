ca1517b386eef5e83681df815e273306f72d1638
aakarsh30 <aakarshsaxenasup@gmail.com>
2020-10-22 18:31:12 +0530

    Updates

---
+++ precache-manifest.5e173ce1142c9dbb97330bbbf43d5c50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03697bef3593b1ea7e1fa302bbd19919",
+    "url": "/videodemo/index.html"
+  },
+  {
+    "revision": "bfbe7f61809b5eb919b4",
+    "url": "/videodemo/static/css/main.30fc2a87.chunk.css"
+  },
+  {
+    "revision": "144306f23a7096284648",
+    "url": "/videodemo/static/js/2.415bb63f.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/videodemo/static/js/2.415bb63f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfbe7f61809b5eb919b4",
+    "url": "/videodemo/static/js/main.eaeace8c.chunk.js"
+  },
+  {
+    "revision": "880a06d460aa0ba97e7a",
+    "url": "/videodemo/static/js/runtime-main.f78c309b.js"
+  }
+]);
